969b3357cd65cecfd5d1e244ec9b703f871103ef
annael.moussa <51157526+annaelmoussa-dev@users.noreply.github.com>
2019-11-25 19:11:12 +0100

    Update Amazon to Z-library.user.js

--- Amazon to Z-library.user.js
+++ Amazon to Z-library.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Add link to libgen on all ebook products
-// @author       Niks
+// @author       Annaël
 // @match        https://www.amazon.*/*
 // @match        https://www.amazon.fr/*
 // @grant        none
